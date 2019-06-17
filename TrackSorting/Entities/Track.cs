8d89d34aaee3e1bbe012e54cbf59e4af5439ea5f
cafenombre <37804355+cafenombre@users.noreply.github.com>
2019-06-17 11:36:54 +0200

    Add files via upload

---
+++ TrackSorting/Entities/Track.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackSorting.Entities
+{
+    class Track
+    {
+        #region properties
+        
+        public string name { get; set; }
+        
+        // More properties like date, album, artist will be added here
+
+        #endregion
+
+        #region constructors
+
+        /// <summary>Basic constructor only with the track name.</summary>
+        /// <param name="trackname">Track name that should be gathered from the Track_List.txt</param>
+        public Track (string trackname)
+        {
+            this.name = trackname;
+        }
+
+        /// <summary>Empty constructor if needed</summary>
+        public Track()
+        {
+            this.name = "";
+        }
+        #endregion
+
+        #region methods
+        #endregion
+
+    }
+}
---
+++ TrackSorting/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TrackSorting.Entities;
+
+namespace TrackSorting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Track testTrack = new Track("What is love ?");
+            // Baby don't hurt me, don't hurt me, no more
+
+            Console.WriteLine("Good Luck !");
+            Console.Read();
+        }
+    }
+}
